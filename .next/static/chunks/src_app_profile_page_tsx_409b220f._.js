5cae3828d0e4abf8dfdad5cd03c0c0db0e4ee137
shiv369tes <144312319+shiv369tes@users.noreply.github.com>
2025-07-18 18:27:37 +0530

    Add .next/static/chunks/src_app_profile_page_tsx_409b220f._.js 1752843456858

---
+++ .next/static/chunks/src_app_profile_page_tsx_409b220f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b613b844._.js",
+  "static/chunks/src_3084e1ae._.js"
+],
+    source: "dynamic"
+});
