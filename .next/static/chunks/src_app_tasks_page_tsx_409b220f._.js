97e9807204424037b223382315ef4b863bfbd7a7
shiv369tes <144312319+shiv369tes@users.noreply.github.com>
2025-07-18 18:27:42 +0530

    Add .next/static/chunks/src_app_tasks_page_tsx_409b220f._.js 1752843461614

---
+++ .next/static/chunks/src_app_tasks_page_tsx_409b220f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_13627380._.js",
+  "static/chunks/node_modules_4d022c4d._.js"
+],
+    source: "dynamic"
+});
